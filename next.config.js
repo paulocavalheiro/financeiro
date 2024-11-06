bb61e2c261569ee8e17f2d75d5f9b5be11df28fc
paulocavalheiro <paulohc.avila@gmail.com>
2024-11-06 16:00:26 -0300

    fix:vercel

--- next.config.js
+++ next.config.js
@@ -6,35 +6,7 @@ const routes = require("./src/routes/moduleRoutes.json");
 module.exports = {
   webpack(config, options) {
     const { isServer } = options;
-    config.plugins.push(
-      new NextFederationPlugin({
-        name: "bime_financeiro",
-        remotes: {
-          // bime_vendas: `${process.env.NEXT_PRIVATE_LOCAL_BIMEVENDAS}${
-          //   isServer ? "ssr" : "chunks"
-          // }/remoteEntry.js`,
-          // host: `${process.env.NEXT_PRIVATE_LOCAL_HOST}${
-          //   isServer ? "ssr" : "chunks"
-          // }/remoteEntry.js`,
-        },
-        filename: "static/chunks/remoteEntry.js",
-        exposes: {
-          "./pages/dashboards/listaFinanceiro":
-            "./src/pages/dashboards/listaFinaceiro.tsx",
-        },
-        force: true,
-        shared: {
-          "@chakra-ui/": {
-            singleton: true,
-            requiredVersion: false,
-          },
-          "@emotion/": {
-            singleton: true,
-            requiredVersion: false,
-          },
-        },
-      })
-    );
+    config.plugins.push();
 
     return config;
   },
