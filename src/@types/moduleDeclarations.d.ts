25217b4b898d6bddd34419a885a96285ff63e3e8
paulocavalheiro <paulohc.avila@gmail.com>
2024-11-06 10:45:58 -0300

    fix:vercel

---
+++ src/@types/moduleDeclarations.d.ts
@@ -0,0 +1,4 @@
+declare module "host/contexts/UserProvider" {
+  export type { UserContextType } from "host/contexts/UserProvider";
+  export { UserProvider, useUser } from "host/contexts/UserProvider";
+}
