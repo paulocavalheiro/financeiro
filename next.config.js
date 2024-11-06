405c5c9923c4866dd7b2778d78f0976e3cea03e4
paulocavalheiro <paulohc.avila@gmail.com>
2024-11-06 11:13:26 -0300

    fix:vercel

--- next.config.js
+++ next.config.js
@@ -6,35 +6,35 @@ const routes = require("./src/routes/moduleRoutes.json");
 module.exports = {
   webpack(config, options) {
     const { isServer } = options;
-    // config.plugins.push(
-    //   new NextFederationPlugin({
-    //     name: "bime_financeiro",
-    //     remotes: {
-    //       bime_vendas: `${process.env.NEXT_PRIVATE_LOCAL_BIMEVENDAS}${
-    //         isServer ? "ssr" : "chunks"
-    //       }/remoteEntry.js`,
-    //       host: `${process.env.NEXT_PRIVATE_LOCAL_HOST}${
-    //         isServer ? "ssr" : "chunks"
-    //       }/remoteEntry.js`,
-    //     },
-    //     filename: "static/chunks/remoteEntry.js",
-    //     exposes: {
-    //       "./pages/dashboards/listaFinanceiro":
-    //         "./src/pages/dashboards/listaFinaceiro.tsx",
-    //     },
-    //     shared: {
-    //       "@chakra-ui/": {
-    //         singleton: true,
-    //         requiredVersion: false,
-    //       },
-    //       "@emotion/": {
-    //         singleton: true,
-    //         requiredVersion: false,
-    //       },
-    //     },
-    //     force: true,
-    //   })
-    // );
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: "bime_financeiro",
+        remotes: {
+          bime_vendas: `${process.env.NEXT_PRIVATE_LOCAL_BIMEVENDAS}${
+            isServer ? "ssr" : "chunks"
+          }/remoteEntry.js`,
+          host: `${process.env.NEXT_PRIVATE_LOCAL_HOST}${
+            isServer ? "ssr" : "chunks"
+          }/remoteEntry.js`,
+        },
+        filename: "static/chunks/remoteEntry.js",
+        exposes: {
+          "./pages/dashboards/listaFinanceiro":
+            "./src/pages/dashboards/listaFinaceiro.tsx",
+        },
+        shared: {
+          "@chakra-ui/": {
+            singleton: true,
+            requiredVersion: false,
+          },
+          "@emotion/": {
+            singleton: true,
+            requiredVersion: false,
+          },
+        },
+        force: true,
+      })
+    );
 
     return config;
   },
--- src/@types/module.d.ts
+++ src/@types/module.d.ts
@@ -4,4 +4,3 @@ declare module "host/contexts/UserProvider" {
   export type { UserContextType } from "host/contexts/UserProvider";
   export { UserProvider, useUser } from "host/contexts/UserProvider";
 }
-declare module "host/contexts/UserProvider";
