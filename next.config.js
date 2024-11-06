f9373a39a01538e60d4e32348a44a3af6e203034
paulocavalheiro <paulohc.avila@gmail.com>
2024-11-06 14:55:45 -0300

    fix:vercel

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true, // Ativa o modo estrito do React, ajudando a detectar potenciais problemas
-  swcMinify: true, // Utiliza o minificador SWC, que é mais rápido e otimizado para Next.js
 };
 
 module.exports = nextConfig;
