e5a15d069030e910b0f6f002d6a909bd2e897241
paulocavalheiro <paulohc.avila@gmail.com>
2024-11-06 16:28:52 -0300

    fix:vercel

--- next.config.js
+++ next.config.js
@@ -1,28 +1,41 @@
 /** @type {import('next').NextConfig} */
 
 const NextFederationPlugin = require("@module-federation/nextjs-mf");
-
-const federationConfig = new NextFederationPlugin({
-  name: "bime_financeiro",
-  remoteType: "module",
-  remotes: {
-    host: async () =>
-      `${process.env.NEXT_PUBLIC_PRODUCTION_HOST}/remoteEntry.js`,
-  },
-  filename: "remoteEntry.js",
-  exposes: {
-    "./pages/dashboards/listaFinanceiro":
-      "./src/pages/dashboards/listaFinaceiro.tsx",
-  },
-  shared: {
-    "@chakra-ui/react": { singleton: true, requiredVersion: false },
-    "@emotion/react": { singleton: true, requiredVersion: false },
-  },
-});
+const routes = require("./src/routes/moduleRoutes.json");
 
 module.exports = {
   webpack(config, options) {
-    config.plugins.push(federationConfig);
+    const { isServer } = options;
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: "bime_financeiro",
+        remotes: {
+          // bime_vendas: `${process.env.NEXT_PRIVATE_LOCAL_BIMEVENDAS}${
+          //   isServer ? "ssr" : "chunks"
+          // }/remoteEntry.js`,
+          host: `${process.env.NEXT_PRIVATE_LOCAL_HOST}${
+            isServer ? "ssr" : "chunks"
+          }/remoteEntry.js`,
+        },
+        filename: "static/chunks/remoteEntry.js",
+        exposes: {
+          "./pages/dashboards/listaFinanceiro":
+            "./src/pages/dashboards/listaFinaceiro.tsx",
+        },
+        force: true,
+        // shared: {
+        //   "@chakra-ui/": {
+        //     singleton: true,
+        //     requiredVersion: false,
+        //   },
+        //   "@emotion/": {
+        //     singleton: true,
+        //     requiredVersion: false,
+        //   },
+        // },
+      })
+    );
+
     return config;
   },
 };
