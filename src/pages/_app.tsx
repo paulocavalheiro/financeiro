cb3bee5f3dec27899d8641ddbafeafdf07bd261a
paulocavalheiro <paulohc.avila@gmail.com>
2024-11-06 16:05:21 -0300

    fix:vercel

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,17 +4,17 @@ import type { AppProps } from "next/app";
 import theme from "@/styles/theme";
 import dynamic from "next/dynamic";
 
-const UserProviderHost = dynamic<{ children: React.ReactNode }>(
-  () => import("host/contexts/UserProvider").then((mod) => mod.UserProvider),
-  { ssr: false }
-);
+// const UserProviderHost = dynamic<{ children: React.ReactNode }>(
+//   () => import("host/contexts/UserProvider").then((mod) => mod.UserProvider),
+//   { ssr: false }
+// );
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <UserProviderHost>
-      <ChakraProvider theme={theme}>
-        <Component {...pageProps} />
-      </ChakraProvider>
-    </UserProviderHost>
+    // <UserProviderHost>
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+    // </UserProviderHost>
   );
 }
