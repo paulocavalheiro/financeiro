cfebde55614ef0b489c873b820b04764d3e885bc
paulocavalheiro <paulohc.avila@gmail.com>
2024-11-06 16:03:57 -0300

    fix:vercel

--- next.config.js
+++ next.config.js
@@ -4,38 +4,37 @@ const NextFederationPlugin = require("@module-federation/nextjs-mf");
 const routes = require("./src/routes/moduleRoutes.json");
 
 module.exports = {
-  webpack(config, options) {
-    const { isServer } = options;
-    config.plugins.push(
-      new NextFederationPlugin({
-        name: "bime_financeiro",
-        remotes: {
-          // bime_vendas: `${process.env.NEXT_PRIVATE_LOCAL_BIMEVENDAS}${
-          //   isServer ? "ssr" : "chunks"
-          // }/remoteEntry.js`,
-          host: `${process.env.NEXT_PRIVATE_LOCAL_HOST}${
-            isServer ? "ssr" : "chunks"
-          }/remoteEntry.js`,
-        },
-        filename: "static/chunks/remoteEntry.js",
-        exposes: {
-          "./pages/dashboards/listaFinanceiro":
-            "./src/pages/dashboards/listaFinaceiro.tsx",
-        },
-        force: true,
-        // shared: {
-        //   "@chakra-ui/": {
-        //     singleton: true,
-        //     requiredVersion: false,
-        //   },
-        //   "@emotion/": {
-        //     singleton: true,
-        //     requiredVersion: false,
-        //   },
-        // },
-      })
-    );
-
-    return config;
-  },
+  // webpack(config, options) {
+  //   const { isServer } = options;
+  //   config.plugins.push(
+  //     new NextFederationPlugin({
+  //       name: "bime_financeiro",
+  //       remotes: {
+  //         // bime_vendas: `${process.env.NEXT_PRIVATE_LOCAL_BIMEVENDAS}${
+  //         //   isServer ? "ssr" : "chunks"
+  //         // }/remoteEntry.js`,
+  //         host: `${process.env.NEXT_PRIVATE_LOCAL_HOST}${
+  //           isServer ? "ssr" : "chunks"
+  //         }/remoteEntry.js`,
+  //       },
+  //       filename: "static/chunks/remoteEntry.js",
+  //       exposes: {
+  //         "./pages/dashboards/listaFinanceiro":
+  //           "./src/pages/dashboards/listaFinaceiro.tsx",
+  //       },
+  //       force: true,
+  //       // shared: {
+  //       //   "@chakra-ui/": {
+  //       //     singleton: true,
+  //       //     requiredVersion: false,
+  //       //   },
+  //       //   "@emotion/": {
+  //       //     singleton: true,
+  //       //     requiredVersion: false,
+  //       //   },
+  //       // },
+  //     })
+  //   );
+  //   return config;
+  // },
 };
